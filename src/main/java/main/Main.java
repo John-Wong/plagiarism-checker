d8712ef048d317ca3183b844e84e789b2960fbfd
John <john_wong@live.com>
2021-09-25 15:49:43 +0800

    v1.1 增加耗时显示、默认输出结果

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,16 +6,21 @@ import tool.TextException;
 import  tool.TextTools;
 import util.TxtIOUtils;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, TextException {
+        long start = System.currentTimeMillis();
         Article a1 = TextTools.getArticle(args[0]);
         Article a2 = TextTools.getArticle(args[1]);
         String resultFilePath = args[2];
         CompareTask task = new CompareTask(a1, a2);
         task.execute();
         TxtIOUtils.writeTxt(task.getCompareReport().toString(), resultFilePath);
+        long end = System.currentTimeMillis();
+        System.out.println("完成！耗时 "+ (end - start) +" 毫秒。");
+        System.out.println(task.getCompareReport().toString());
         System.exit(0);
     }
 }
